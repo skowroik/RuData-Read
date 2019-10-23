f064a0d1ceccb7fa4829d60b561bd594729239aa
Jakub Skowronski <kuba@skowroik>
2019-10-23 17:27:58 +0200

    Small changes (better indices)

--- Event.cc
+++ Event.cc
@@ -13,17 +13,15 @@ using namespace std;
 
 struct Event {
 
- public:
+  unsigned short timeStamp [3];           // timestamp 48 bit
+  unsigned short sampleNumber;            // sample number 16 bit
+  unsigned short frameLenght;             // frame lenght 16 bit
+  unsigned short statusHead;              // status 16 bit
 
-  unsigned short timeStamp [3];         // timestamp 48 bit
-  unsigned short sampleNumber;          // sample number 16 bit
-  unsigned short frameLenght;           // frame lenght 16 bit
-  unsigned short statusHead;            // status 16 bit
+  unsigned long energy [36];              // energy array for every channel, 32 bit each
+  unsigned short statusChan [36];         // status array for every channel, 16 bit each
 
-  unsigned long energy [36];                 // energy array for every channel, 32 bit each
-  unsigned short statusChan [36];       // status array for every channel, 16 bit each
-
-  unsigned short waveSignal [36][99];       // sample data
+  unsigned short waveSignal [36][99];     // sample data
 
 };
 
@@ -40,7 +38,7 @@ void readRuData(){
   file.open( "rudata_gal-09_ru4_s87_r4036.i1", std::ios::binary );                  // opening the file
 
   //  ofstream output;
-  //  output.open( "out_prova" );                                                       // an output to print whatever
+  //  output.open( "out_prova" );                                                     // an output to print whatever
 
   Event* ev;                                                                        // creating an Event
   char bin [ EV_SIZE*2 ];                                                           // char array to contain binary data (multiply by 2 because every word is 16 bit, not 8)
@@ -57,9 +55,9 @@ void readRuData(){
 
     TTree *tree = new TTree("T","An example of a ROOT tree");
 
-    file.read( bin , sizeof( bin ) );                                              // reading binary data
+    file.read( bin , sizeof( bin ) );                                               // reading binary data
     
-    memcpy( &ev->timeStamp, bin + 6, sizeof( ev->timeStamp ) );
+    memcpy( &ev->timeStamp, bin + 6, sizeof( ev->timeStamp ) );                     // associating variables to Event struct
     memcpy( &ev->sampleNumber, bin + 12, sizeof( ev->sampleNumber ) );
     memcpy( &ev->frameLenght, bin + 14, sizeof( ev->frameLenght ) );
     memcpy( &ev->statusHead, bin + 18, sizeof( ev->statusHead ) );
@@ -71,9 +69,9 @@ void readRuData(){
       memcpy( &ev->statusChan[i], bin + 36 + ( i*108*2 ), sizeof( ev->statusChan[i] ) );
 
       int k;
-      for( k = 2; k < 200; k = k + 2 ){
+      for( k = 1; k < 100; k = k + 1 ){
 
-	memcpy( &ev->waveSignal[i][ ( ( k/2 ) - 1 ) ], bin + 48 + k + ( i*108*2 ), sizeof( ev->waveSignal[i][ ( ( k/2 ) - 1 ) ] ) );
+	memcpy( &ev->waveSignal[i][ ( k  - 1 ) ], bin + 48 + k*2 + ( i*108*2 ), sizeof( ev->waveSignal[i][ ( k  - 1 ) ] ) );
 
       }
       
